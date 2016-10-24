cf3e45daad3acbcf606578e04b1de159218da511
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:24:14 +0800

    rf, simplify the logic and expand the condition range

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -1,14 +1,11 @@
 export default (door)=>{
   const result = [1]
   let candidate = 2
-  while(candidate < door){
+  while(candidate <= door){
     if(door % candidate === 0){
       result.push(candidate)
     }
     candidate++
-  }
-  if(door > 1){
-    result.push(door)
   }
   return result
 }
