d8ce54f95d8e7be3ae571422a75899665b2cbf76
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:41:53 +0800

    ut, door state ut param should be 1

--- test/getDoorState.test.js
+++ test/getDoorState.test.js
@@ -2,6 +2,6 @@ import {expect} from 'chai'
 
 describe('door state', ()=>{
   it('should be true for door 1', ()=>{
-    expect(getDoorState([1])).to.be.true
+    expect(getDoorState(1)).to.be.true
   })
 })
