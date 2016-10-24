da61430d8a3183701c3d838712ae13a00ca705f0
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 16:46:33 +0800

    ut, for door 1, failed due to no getDoorTogglePoints function

--- test/getDoorTogglePoints.test.js
+++ test/getDoorTogglePoints.test.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai'
 
 describe('door toggle points',()=>{
-  it('should be true for true', ()=>{
-    expect(true).to.be.true
+  it('should be [1] for door 1', ()=>{
+    expect(getDoorTogglePoints(1)).to.be.eql([1])
   })
 })
