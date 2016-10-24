56113703380a4e474c50332093d8131cec8ea814
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:22:21 +0800

    rf, make the multiple if into one while

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -1,16 +1,11 @@
 export default (door)=>{
   const result = [1]
   let candidate = 2
-  if(door % candidate === 0 && door !== candidate){
-    result.push(candidate)
-  }
-  candidate++
-  if(door % candidate === 0 && door !== candidate){
-    result.push(candidate)
-  }
-  candidate++
-  if(door % candidate === 0 && door !== candidate){
-    result.push(candidate)
+  while(candidate < door){
+    if(door % candidate === 0 && door !== candidate){
+      result.push(candidate)
+    }
+    candidate++
   }
   if(door > 1){
     result.push(door)
