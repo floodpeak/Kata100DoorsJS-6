ba44b86b3d9f63329ea18f4c0b52d3ee1caaea26
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:26:27 +0800

    rf, remove the unnessasary braces

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -1,9 +1,7 @@
 export default (door)=>{
   const result = [1]
-  for(let candidate = 2;candidate <= door;candidate++){
-    if(door % candidate === 0){
+  for(let candidate = 2;candidate <= door;candidate++)
+    if(door % candidate === 0)
       result.push(candidate)
-    }
-  }
   return result
 }
