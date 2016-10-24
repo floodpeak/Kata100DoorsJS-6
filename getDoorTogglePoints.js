eea4afa3e9e92e6d528d21438d02861b2acb65c5
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:10:22 +0800

    rf, make door 8 use result as return value

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -1,7 +1,4 @@
 export default (door)=>{
-  if(door === 8){
-    return [1,2,4,8]
-  }
   const result = [1]
   const candidate = 2
   if(door % candidate === 0 && door !== candidate){
@@ -9,6 +6,9 @@ export default (door)=>{
   }
   if(door === 6){
     result.push(3)
+  }
+  if(door === 8){
+    result.push(4)
   }
   if(door > 1){
     result.push(door)
