996ae757fbe5a7b610d484aaec38c673ba21b787
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:00:24 +0800

    rf, combine the same part of door 4 and door 2and 3 logic

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -2,9 +2,8 @@ export default (door)=>{
   const result = [1]
   if(door === 4){
     result.push(2)
-    result.push(door)
   }
-  if(door === 2 || door === 3){
+  if(door === 2 || door === 3 || door === 4){
     result.push(door)
   }
   return result
