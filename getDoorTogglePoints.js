d60dd66fcdb6da492ed7e7610d161e629ab94a2c
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:25:57 +0800

    rf, change the while into for, simpler

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -1,11 +1,9 @@
 export default (door)=>{
   const result = [1]
-  let candidate = 2
-  while(candidate <= door){
+  for(let candidate = 2;candidate <= door;candidate++){
     if(door % candidate === 0){
       result.push(candidate)
     }
-    candidate++
   }
   return result
 }
