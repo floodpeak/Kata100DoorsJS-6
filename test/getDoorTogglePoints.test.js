86bf12faca397a2accd2f3d93ec2ed3b3cf88010
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 16:49:16 +0800

    ut, for door 2, failed

--- test/getDoorTogglePoints.test.js
+++ test/getDoorTogglePoints.test.js
@@ -5,4 +5,7 @@ describe('door toggle points',()=>{
   it('should be [1] for door 1', ()=>{
     expect(getDoorTogglePoints(1)).to.be.eql([1])
   })
+  it('should be [1,2] for door 2', ()=>{
+    expect(getDoorTogglePoints(2)).to.be.eql([1,2])
+  })
 })
