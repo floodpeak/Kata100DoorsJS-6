55bf5717d3977ead08900089cf522aea7cc846f7
floodpeak <hongfengzhou@yahoo.com>
2016-10-24 17:20:09 +0800

    rf, make the door 6 logic and condition look exactly the same to door 4

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -5,7 +5,7 @@ export default (door)=>{
     result.push(candidate)
   }
   candidate++
-  if(door === 6){
+  if(door % candidate === 0 && door !== candidate){
     result.push(candidate)
   }
   if(door === 8){
